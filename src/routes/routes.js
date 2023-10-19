4a8388fb553f2256650cfe1a824d1d25ed10933e
Anagha P <anagha.p@coddletech.com>
2023-10-19 09:49:25 +0530

    new commit

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userController = require('../controllers/user');
 const menuController = require('../controllers/menu');
+const ordersController = require('../controllers/orders');
 const authMiddleware = require('../middlewares/authenticate');
 
 const router = express.Router();
@@ -8,5 +9,6 @@ const router = express.Router();
 router.post('/signup', userController.signup);
 router.post('/login', userController.login);
 router.get('/fetchmenu', authMiddleware.verifyToken, menuController.fetchmenu);
+router.get('/fetchorder', authMiddleware.verifyToken, ordersController.fetchOrders);
 
 module.exports = router;
