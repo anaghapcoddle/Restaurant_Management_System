421d15ed76a445b994aa94f13ba1d2db4b863cce
Anagha P <anagha.p@coddletech.com>
2023-11-10 16:11:56 +0530

    new commit

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -3,19 +3,23 @@ const jwt = require('jsonwebtoken');
 function authorizePage(permissions) {
   return (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
-    let userRole;
 
-    jwt.verify(token, 'secret', (err, decoded) => {
-      if (err) {
-        res.status(401).send({ error: 'Authentication failed' });
-      } else {
-        userRole = decoded.role;
-        console.log(userRole);
-        if (permissions.includes(userRole)) {
-          next();
+    jwt.verify(token, 'secret', async (err, decoded) => {
+      try {
+        if (err) {
+          res.status(401).send({ error: 'Authentication failed' });
         } else {
-          return res.status(401).json('You do not have permissions');
+          const userRole = decoded.role;
+          console.log(userRole);
+
+          if (permissions.includes(userRole)) {
+            next();
+          } else {
+            return res.status(401).json('You do not have permissions');
+          }
         }
+      } catch (error) {
+        res.status(401).send({ error: 'Authentication failed' });
       }
     });
   };
